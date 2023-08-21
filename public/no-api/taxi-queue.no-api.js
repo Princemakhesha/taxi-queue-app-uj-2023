104d494172b6e19315bb82b14d43f903af181d2a
Prince Makhesha <princemakhesha@gmail.com>
2023-08-21 14:07:08 +0200

    done with no-api app

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -3,49 +3,46 @@ document.addEventListener('alpine:init', () => {
 	Alpine.data('TaxiQueue', () => {
 
 		return {
-			version: 'no-api-1.0'
+			version: 'no-api-1.0',
+			passengerQueue: 0,
+			taxiQueue: 0,
+
+			joinQueue() {
+				this.passengerQueue++;
+
+			},
+			leaveQueue() {
+				if (this.passengerQueue > 0) {
+					this.passengerQueue--;
+				}
+
+			},
+
+			joinTaxiQueue() {
+				this.taxiQueue++;
+
+			},
+
+			queueLength() {
+				return this.passengerQueue;
+
+
+			},
+
+			taxiQueueLength() {
+				return this.taxiQueue;
+
+			},
+
+			taxiDepart() {
+				if (this.passengerQueue >= 12 && this.taxiQueue > 0) {
+					this.taxiQueue--;
+					this.passengerQueue -= 12;
+				}
+
+			}
 		}
 
 	});
 
-
-	function TaxiQueue() {
-
-
-		function joinQueue() {
-	
-		}
-	
-		function leaveQueue() {
-	
-		}
-	
-		function joinTaxiQueue() {
-	
-		}
-	
-		function queueLength() {
-	
-		}
-	
-		function taxiQueueLength() {
-	
-		}
-	
-		function taxiDepart(){
-	
-		}
-	
-		return {
-			joinQueue,
-			leaveQueue,
-			joinTaxiQueue,
-			queueLength,
-			taxiQueueLength,
-			taxiDepart
-		}
-	}
-
 });
-
-
