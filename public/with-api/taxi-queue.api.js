c67af6af2202fb45eab3b8de25bd39d2002e469e
Prince Makhesha <princemakhesha@gmail.com>
2023-08-21 15:56:08 +0200

    chasing due-time

--- public/with-api/taxi-queue.api.js
+++ public/with-api/taxi-queue.api.js
@@ -5,37 +5,61 @@ document.addEventListener('alpine:init', () => {
 		return {
 			version: 'api-1.0',
 			queueLength: 0,
+			taxiQueueLength: 0,
+
 			init() {
-				axios
-					.get('/api/passenger/queue')
-					.then(result => {
-						// an example API call
-						this.queueLength = result.data.queueCount
-					});
+				this.queueLength();
+				this.taxiQueueLength();
 			},
 			joinQueue() {
-
+				axios
+					.post('/api/passenger/join')
+					.then(() => {
+						this.queueLength();
+					});
 			},
-			leaveQueue() {
 
+			leaveQueue() {
+				axios
+					.post('/api/passenger/leave')
+					.then(() => {
+						this.queueLength();
+					});
 			},
 
 			joinTaxiQueue() {
-
+				axios
+					.post('/api/taxi/join')
+					.then(() => {
+						this.taxiQueueLength();
+					});
 			},
-
 			queueLength() {
-
+				axios
+					.get('/api/passenger/queue')
+					.then(result => {
+						this.queueLength = result.data.queueCount;
+					});
 			},
-
 			taxiQueueLength() {
-
+				axios
+					.get('/api/taxi/queue')
+					.then(result => {
+						this.taxiQueueLength = result.data.queueCount;
+					});
 			},
-
 			taxiDepart() {
-
+				if (this.queueLength >= 12) {
+					axios
+						.post('/api/taxi/depart')
+						.then(() => {
+							this.taxiQueueLength();
+							this.queueLength(); // Update people queue length after taxi departure
+						});
+				}
 			}
-		}
+		};
 	});
 
 });
+
--- taxi.sql.js
+++ taxi.sql.js
@@ -1,34 +1,44 @@
 import * as sqlite from 'sqlite';
 import sqlite3 from 'sqlite3';
 
-const  db = await sqlite.open({
-    filename:  './taxi_queue.db',
-    driver:  sqlite3.Database
+const db = await sqlite.open({
+    filename: './taxi_queue.db',
+    driver: sqlite3.Database
 });
 
 await db.migrate();
 
 export async function joinQueue() {
     // console.log('join queue')
+    await db.run('UPDATE taxi_queue SET passenger_queue_count = passenger_queue_count + 1');
 
 }
 
 export async function leaveQueue() {
-    
+    await db.run('UPDATE taxi_queue SET passenger_queue_count = passenger_queue_count - 1 WHERE passenger_queue_count > 0 LIMIT 1');
+
 }
 
 export async function joinTaxiQueue() {
-   
+    await db.run('UPDATE taxi_queue SET taxi_queue_count = taxi_queue_count + 1');
+
 }
 
 export async function queueLength() {
-       
+    const row = await db.get('SELECT SUM(passenger_queue_count) as total FROM taxi_queue');
+    return row.total || 0;
+
 }
 
 export async function taxiQueueLength() {
+    const row = await db.get('SELECT taxi_queue_count FROM taxi_queue');
+    return row.taxi_queue_count || 0;
 
 }
 
 export function taxiDepart() {
-
+    const taxiRow = db.get('SELECT taxi_queue_count FROM taxi_queue');
+    if (taxiRow.taxi_queue_count > 0) {
+     db.run(`UPDATE taxi_queue SET taxi_queue_count = taxi_queue_count - 1`);
+    }
 }
